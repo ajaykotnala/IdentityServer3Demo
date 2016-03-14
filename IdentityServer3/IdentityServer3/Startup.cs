a94894d850fc9a046e1f50d20cab83f034e87786
ajay.kotnala <ajay.kotnala@aveva.com>
2016-03-14 10:55:37 +0530

    first commit to code still not running

---
+++ IdentityServer3/IdentityServer3/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using IdentityServer3.Configuration;
+using Microsoft.Owin;
+using Owin;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Core.Models;
+
+[assembly: OwinStartup(typeof(IdentityServer3.Startup))]
+
+namespace IdentityServer3
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.Map("/identity", idsrvApp =>
+            {
+                idsrvApp.UseIdentityServer(new IdentityServerOptions
+                {
+                    SiteName = "Embedded IdentityServer",
+                    SigningCertificate = LoadCertificate(),
+
+                    Factory = InMemoryFactory.Create(
+                        users: Users.Get(),
+                        clients: Clients.Get(),
+                        scopes: StandardScopes.All)
+                });
+            });
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                $@"{AppDomain.CurrentDomain.BaseDirectory}\bin\identityServer\idsrv3test.pfx", "idsrv3test");
+        }
+    }
+}
